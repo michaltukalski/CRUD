c63587efd54862fe1c88e6d05a715380feb3e726
Michał Tukalski <michaltukalski@gmail.com>
2017-03-26 16:41:37 +0200

    poczatki

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.listen(3000,  () => {
-  console.log(`Hello WOrld - im server, im up and running on port ${port}`)
+  console.log(`Hello NEW WOrld - im server, im up and running on port ${port}`)
 })
 
-app.get('/', (request, response)=>{
-  response.send('wszystko ok');
+app.get('/', (req, res)=>{
+  console.log(__dirname)
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/titles', (req, res)=> {
+  console.log('2 Wait for titles');
 })
