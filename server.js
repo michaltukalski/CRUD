c9429494974612606813492c3575a66131732c04
Michał Tukalski <michaltukalski@gmail.com>
2017-03-26 15:40:35 +0200

    create URL response

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const app = express();
 const port = 3000;
 app.listen(3000,  () => {
   console.log(`Hello WOrld - im server, im up and running on port ${port}`)
+})
 
+app.get('/', (request, response)=>{
+  response.send('wszystko ok');
 })
