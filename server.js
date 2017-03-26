8e576ad53d76e3ef4224a85dc2969d2f1e0d62db
Michał Tukalski <michaltukalski@gmail.com>
2017-03-26 15:25:09 +0200

    modify package json and server listen on port

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello WOrld")
+const express = require("express");
+const app = express();
+const port = 3000;
+app.listen(3000,  () => {
+  console.log(`Hello WOrld - im server, im up and running on port ${port}`)
+
+})
