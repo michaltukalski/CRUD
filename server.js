fa185977f02eb08d57f9cbf1aa63f70d8939bf6f
Michał Tukalski <michaltukalski@gmail.com>
2017-04-01 12:56:24 +0200

    poczatki-podpieta baza

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const BookSchema = require ('./bookSchema');
 
 const app = express();
 const port = 3000;
 let db;
 
 app.use(bodyParser.urlencoded({encode: true, extended:true}));
+
+app.set('view engine', 'ejs');
 
 MongoClient.connect('mongodb://tukal:aaa@ds149040.mlab.com:49040/book', (err, res)=>{
   if(err){
@@ -17,15 +20,26 @@ MongoClient.connect('mongodb://tukal:aaa@ds149040.mlab.com:49040/book', (err, re
     console.log(`Server is up and running on port ${port}`);
   })
 })
-// app.listen(3000,  () => {
-//   console.log(`Hello NEW WOrld - im server, im up and running on port ${port}`)
-// })
 
 app.get('/', (req, res)=>{
+  db.collection('book').find().toArray((err, result) => {
+    console.log(result);
+    res.render('index.ejs', {titles: result});
+  });
   console.log(__dirname)
-  res.sendFile(__dirname + '/index.html');
-})
+});
 
 app.post('/titles', (req, res)=> {
-  console.log('2 Wait for titles');
+  const book = new BookSchema;
+  book.title = req.body.title;
+  book.description = req.body.description;
+  db.collection('book').save(book, (err, result)=>{
+    console.log(req.body);
+    if(err){
+      return console.error(err);
+    }
+    console.info('Saved to database');
+    res.redirect('/');;
+  })
+
 })
