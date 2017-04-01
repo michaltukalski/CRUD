711c09bb2d35ce4c7bca16c4141c146eff102f98
Michał Tukalski <michaltukalski@gmail.com>
2017-04-01 09:43:21 +0200

    poczatki-podpieta baza

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 const port = 3000;
-app.listen(3000,  () => {
-  console.log(`Hello NEW WOrld - im server, im up and running on port ${port}`)
+let db;
+
+app.use(bodyParser.urlencoded({encode: true, extended:true}));
+
+MongoClient.connect('mongodb://tukal:aaa@ds149040.mlab.com:49040/book', (err, res)=>{
+  if(err){
+    return console.error(err);
+  }
+  db = res;
+  app.listen(port, ()=>{
+    console.log(`Server is up and running on port ${port}`);
+  })
 })
+// app.listen(3000,  () => {
+//   console.log(`Hello NEW WOrld - im server, im up and running on port ${port}`)
+// })
 
 app.get('/', (req, res)=>{
   console.log(__dirname)
